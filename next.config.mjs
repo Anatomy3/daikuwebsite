b3112fd60f12b5d65b7748872561d72ff49ab304
Vigio <linggacrack@gmail.com>
2024-12-03 14:43:01 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,22 @@
 // next.config.mjs
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**',
-        },
-      ],
-    },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   }
-  
-  export default nextConfig;
+}
+
+export default nextConfig;
