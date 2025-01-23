36f035121d4613842a6d3a64c2abb3fb3a90719f
Vigio <daikudata@gmail.com>
2025-01-23 22:07:21 +0700

    up

--- app/aboutus/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/aboutus/page.tsx
-import { AboutUs } from '@/components/AboutUs';
-
-export default function AboutPage() {
-  return <AboutUs />;
-}
--- utils/stringUtils.ts
+++
@@ -1,8 +0,0 @@
-// utils/stringUtils.ts
-export function createSlug(text: string): string {
-    return text
-      .toLowerCase()
-      .replace(/[^a-z0-9]+/g, '-') // Ganti karakter non-alphanumeric dengan dash
-      .replace(/^-+|-+$/g, '')     // Hapus dash di awal dan akhir
-      .trim();
-  }
